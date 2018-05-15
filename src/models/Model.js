181988783accd21700a7565b16f4da396933a4b6
Daniel Flow <thedanielflow@gmail.com>
2018-05-15 17:24:01 +0300

    add base model and collection

---
+++ src/models/Model.js
@@ -0,0 +1,17 @@
+import {
+  API_GATE_ENDPOINT
+} from '@/api/config'
+import { Model } from 'vue-mc'
+
+export default class extends Model {
+  getRequest (config) {
+    config.url = API_GATE_ENDPOINT + config.url
+    config.headers = {
+      common: {
+        'X-Auth-Token': localStorage.getItem('_token')
+      }
+    }
+
+    return super.getRequest(config)
+  }
+}
