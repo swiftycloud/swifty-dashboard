0e861874b1f79474c334b2426752588680baf947
Daniel Flow <thedanielflow@gmail.com>
2018-06-21 15:26:02 +0300

    add middleware model and collection

---
+++ src/models/Middleware.js
@@ -0,0 +1,44 @@
+import Model from '@/models/Model'
+import Collection from '@/models/Collection'
+
+export class Middleware extends Model {
+  defaults () {
+    return {
+      id: null,
+      name: null,
+      type: null
+    }
+  }
+
+  routes () {
+    return {
+      fetch: '/middleware/{id}',
+      save: '/middleware',
+      delete: '/middleware/{id}'
+    }
+  }
+}
+
+export class MiddlewareList extends Collection {
+  model () {
+    return Middleware
+  }
+
+  defaults () {
+    return {
+      project: 'default',
+      details: false,
+      type: false
+    }
+  }
+
+  routes () {
+    return {
+      fetch: '/middleware?project={project}&details={details}&type={type}'
+    }
+  }
+
+  get deactivated () {
+    return this.state === 'deactivated'
+  }
+}
