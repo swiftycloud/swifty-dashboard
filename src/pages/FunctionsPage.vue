90274263f5760635290b03c609a71d4f77a745d5
Daniel Flow <thedanielflow@gmail.com>
2018-07-08 17:20:48 +0300

    fix lastcall info

--- src/pages/FunctionsPage.vue
+++ src/pages/FunctionsPage.vue
@@ -111,13 +111,13 @@ Contact: info@swifty.cloud
             sortable>
             <template slot-scope="scope">
               <el-tooltip
-                v-if="scope.row.lastcall"
+                v-if="scope.row.stats[0].lastcall"
                 effect="dark"
-                :content="scope.row.lastcall | moment('YYYY-MM-DD HH:mm:ss')"
+                :content="scope.row.stats[0].lastcall | moment('YYYY-MM-DD HH:mm:ss')"
                 placement="right">
-                <span v-if="'lastcall' in scope.row">{{ scope.row.lastcall | moment('from', 'now') }}</span>
+                <span v-if="'lastcall' in scope.row.stats[0]">{{ scope.row.stats[0].lastcall | moment('from', 'now') }}</span>
               </el-tooltip>
-              <span v-if="scope.row.lastcall === undefined">never</span>
+              <span v-if="!('lastcall' in scope.row.stats[0])">never</span>
             </template>
           </el-table-column>
         </el-table>
