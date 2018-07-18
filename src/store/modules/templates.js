c157fb9f11aa0b2d85b7e9f821ac9ad2ecc35f56
admin <admin@vporokhov.com>
2018-07-18 16:37:10 +0300

    update templates

--- src/store/modules/templates.js
+++ src/store/modules/templates.js
@@ -20,8 +20,8 @@ export default {
         name: 'Hello World',
         description: 'Simple hello world function, returning a "Hello, $user" string taking $user from the "name" argument',
         language: 'python',
-        source: 'def main(args):\n' +
-                '    return {"message": "Hello, %s" % args[\'name\']}',
+        source: 'def Main(req):\n' +
+                '    return {"message": "hw:python:%s" % req.args[\'name\']}, None',
         args: { name: 'username' }
       },
 
@@ -30,18 +30,29 @@ export default {
         name: 'Maria DB Select',
         description: 'Sample SQL function, that executes SELECT query on a given Maria database and the table name',
         language: 'python',
-        source: 'import swifty\n' +
+        source: 'import pymysql.cursors\n' +
+                'import os\n' +
+                'import swifty\n' +
                 '\n' +
-                'def main(args):\n' +
-                '    db = swifty.MariaConn(args[\'db\'])\n' +
+                'def Main(req):\n' +
+                '    db = swifty.MariaConn(req.args[\'dbname\'])\n' +
+                '    res = "invalid"\n' +
                 '    with db.cursor() as cursor:\n' +
-                '        cursor.execute("SELECT * FROM %s", (args[\'table\'],))\n' +
-                '        res = cursor.fetchone()\n' +
-                '    return res',
+                '        if req.args[\'action\'] == \'create\':\n' +
+                '           cursor.execute("CREATE TABLE `data` (`key` VARCHAR(64), `val` VARCHAR(64))")\n' +
+                '            res = "done"\n' +
+                '        elif req.args[\'action\'] == \'insert\':\n' +
+                '            cursor.execute("INSERT INTO `data` (`key`, `val`) VALUES (%s, %s)", (req.args[\'key\'], req.args['val']))\n' +
+                '            res = "done"\n' +
+                '        elif req.args[\'action\'] == \'select\':\n' +
+                '            cursor.execute("SELECT `val` FROM `data` WHERE `key` = %s", (req.args[\'key\'],))\n' +
+                '            res = cursor.fetchone()[\'val\']\n' +
+                '    db.commit()\n' +
+                '    return { "res": res }, None',
         args: {
-          db: '<ID of mware attached>',
-          table: 'mytable'
-        }
+            db: '<ID of mware attached>',
+            table: 'mytable'
+          }
       },
 
       {
@@ -49,14 +60,24 @@ export default {
         name: 'MongoDB Lookup',
         description: 'Sample lookup in a given Mongo database and collection',
         language: 'python',
-        source: 'import swifty\n' +
+        source: 'from pymongo import MongoClient\n' +
+                'import os\n' +
+                'import swifty\n' +
                 '\n' +
-                'def main(args):\n' +
-                '    db = swifty.MongoDatabase(args[\'db\'])\n' +
-                '    colname = args[\'collection\']\n' +
+                'def Main(req):\n' +
+                '    db = swifty.MongoDatabase(req.args[\'dbname\'])\n' +
+                '    colname = req.args[\'collection\']\n' +
                 '    col = db[colname]\n' +
                 '\n' +
-                '    return col.find_one({ "key": args[\'key\'] })',
+                '    if req.args[\'action\'] == \'insert\':\n' +
+                '        col.insert_one({ "key": req.args[\'key\'], "val": req.args[\'val\'] })\n' +
+                '        return { "res": "done" }\n' +
+                '\n' +
+                '    if req.args[\'action\'] == \'select\':\n' +
+                '        res = col.find_one({ "key": req.args[\'key\'] })\n' +
+                '        return { "res": res[\'val\'] }\n' +
+                '\n' +
+                '    return { "res": "invalid" }, None',
         args: {
           db: '<ID of mongo mware>',
           collection: 'mycollection',
@@ -69,8 +90,8 @@ export default {
         name: 'Hello World',
         description: 'Simple hello world function, returning a "Hello, $user" string taking $user from the "name" argument',
         language: 'swift',
-        source: 'func Main(args: [String:String]) -> Encodable {\n' +
-                '    return ["message": "hw:swift:" + args["name"]!]\n' +
+        source: 'func Main(rq: Request) -> Encodable {\n' +
+                '    return ["message": "hw:swift:" + rq.args!["name"]!]\n' +
                 '}',
         args: { name: 'username' }
       },
@@ -82,8 +103,8 @@ export default {
         language: 'golang',
         source: 'package main\n' +
                 '\n' +
-                'func Main(args map[string]string) interface{} {\n' +
-                '    return ""\n' +
+                'func Main(rq *Request) (interface{}, *Responce) {\n' +
+                '    return map[string]string{"message": "ok"}, nil\n' +
                 '}',
         args: {}
       }
