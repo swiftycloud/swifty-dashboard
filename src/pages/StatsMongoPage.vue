ae48369f1a2ed0f57bc419d591aa2b7c47f0d3ba
Suinly <suinly@suinly.com>
2018-09-23 14:09:20 +0300

    add mwares stats

--- src/pages/StatsMongoPage.vue
+++ src/pages/StatsMongoPage.vue
@@ -11,6 +11,19 @@ Contact: info@swifty.cloud
 
     <el-row>
       <el-col :span="24">
+        <el-form label-width="170px" class="timeline-form">
+          <el-form-item label="Timeline">
+            <el-select placeholder="Timeline" v-model="periods">
+              <el-option v-for="(value, label) in $store.getters.getPeriods" :label="label" :value="value" :key="value"></el-option>
+            </el-select>
+            <span class="period">from {{ periodFrom | moment('L') }} to {{ periodTill | moment('L') }}</span>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="24">
         <el-table :data="mongoStats" style="width: 100%">
           <el-table-column prop="name" label=""></el-table-column>
           <el-table-column prop="period" label="All Period"></el-table-column>
@@ -33,30 +46,30 @@ export default {
       mongoStats: [
         { name: 'Used Storage, GB', period: 0, limit: null },
         { name: 'Number of databases', period: 0, limit: null }
-      ]
+      ],
+
+      periods: 0,
+      periodFrom: null,
+      periodTill: null
     }
   },
 
   created () {
     this.$store.dispatch('setStatsActiveTab', 'mongodb')
-    this.fetchMwareStats()
+    this.fetchStats()
   },
 
   methods: {
-    fetchMwareStats () {
+    fetchStats () {
       this.loading = true
-      this.$store.dispatch('fetchMiddlewareList', {
-        type: 'mongo',
-        project: this.$store.getters.project
-      }).then(response => {
-        if (response.data !== null) {
-          response.data.forEach(item => {
-            if (item.type === 'mongo') {
-              this.mongoStats[0].period += item.disk_usage || 0
-              this.mongoStats[1].period += 1
-            }
-          })
+      this.$store.dispatch('getStats', { periods: this.periods }).then(response => {
+        if ('mongo' in response.data.mware) {
+          this.mongoStats[0].period = response.data.mware.mongo.disk_usage
+          this.mongoStats[1].period = response.data.mware.mongo.count
         }
+
+        this.periodFrom = response.data.stats[0].from ? response.data.stats[0].from : this.$store.state.auth.user.created
+        this.periodTill = response.data.stats[0].till ? response.data.stats[0].till : new Date()
       }).finally(() => {
         this.loading = false
       })
