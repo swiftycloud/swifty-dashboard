77618c2b11fcc7b451678154535e2245bb0283ed
danielflow <thedanielflow@gmail.com>
2018-11-26 23:38:23 +0300

    update logo

--- src/components/NavbarMini.vue
+++ src/components/NavbarMini.vue
@@ -8,7 +8,9 @@ Contact: info@swifty.cloud
 <template>
   <div class="navbar">
     <div class="navbar-brand-block">
-      <a href="/" class="navbar-brand">swifty.</a>
+      <a href="/" class="navbar-brand">
+        <img src="/static/logo-white.svg">
+      </a>
     </div>
   </div>
 </template>
